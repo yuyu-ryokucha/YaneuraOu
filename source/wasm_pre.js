9fad10515e2d653f99450c0c012f445711228ab3
YUYU <110108254+yuyu-ryokucha@users.noreply.github.com>
2026-02-27 01:20:13 +0900

    Revert changes back to 5e1b8eb: initial state before console.log was modified

--- source/wasm_pre.js
+++ source/wasm_pre.js
@@ -5,11 +5,15 @@
   var listeners = [];
 
   Module["print"] = function (line) {
-    if (listeners.length === 0) console.log(line);
-    else
-      setTimeout(function () {
-        for (var i = 0; i < listeners.length; i++) listeners[i](line);
-      });
+    if (listeners.length === 0) {
+      if (typeof postMessage === 'function') {
+        postMessage({ type: 'stdout', text: line });
+      } else {
+        console.log(line);
+      }
+    } else {
+      for (var i = 0; i < listeners.length; i++) listeners[i](line);
+    }
   };
 
   Module["addMessageListener"] = function (listener) {
